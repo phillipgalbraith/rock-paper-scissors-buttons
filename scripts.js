ea653e4d18a87262c4e0f8969d8bb62df0cf7128
topstock <fillupgalbraith@gmail.com>
2021-05-03 08:06:57 -0700

    First commit

--- scripts.js
+++ scripts.js
@@ -1,6 +1,11 @@
 
 //Declare Variable score to store the wins, default zero (0)
 let score = 0;
+// and the rounds played
+let rounds = 0;
+// thirdly, the results of a round is a string
+let results = "";
+
 //Declare FUNCTION computerPlay Parameters:(none)
 function computerPlay() {
 //computerSelector takes one of three random values: = Randomly select 0,1,2  
@@ -21,74 +26,116 @@ function computerPlay() {
 //END
 }
 
-//Delare FUNCTION playOneRound (playerSelection, computerSelection)
-function playOneRound(playerSelection, computerSelection) {
-//  Firstly make playerSelection case-insensitive
-  //    use Slice to put the first letter in a Variable
-      //  toUpperCase the first letter
-    // I made this for another exercise
-  initialLetter = playerSelection.slice(0,1);
-  capitalizedInitial = initialLetter.toUpperCase();
-  noInitial = playerSelection.slice(1);
-  lowerCaseWhatsLeft = noInitial.toLowerCase(noInitial);
-  playerSelection = capitalizedInitial + lowerCaseWhatsLeft;
-// Give an error message or require a incorrect response
-    //IF playerSelection != Rock and playerSelection != Paper and playerSelection != Scissors
-    if (playerSelection != "Rock" && playerSelection != "Paper" && playerSelection != "Scissors") {
-      //  THEN Prompt "No, Really, Rock, Paper or Scissors?"
-      return ("You Lose! " + computerSelection + " beats your answer. Check your spelling next time");
-    //ELSE IF playerSelection is Rock and computerSelection is Paper
-    } else if (playerSelection == "Rock" && computerSelection == "Paper") { 
-    //  THEN winOrLose = ("You Lose! Paper beats Rock")
-      return "You Lose! Paper beats Rock";
-    //ELSE IF playerSelection is Scissors and computerSelection is Rock
+//Delare FUNCTION playRound (playerSelection, computerSelection)
+function playRound(playerSelection, computerSelection) {
+if (rounds > 4) {rounds = 0; score = 0};
+//count this round
+ rounds = ++rounds
+    //player Loss
+    if (playerSelection == "Rock" && computerSelection == "Paper") { 
+      results = "You Lose! Paper beats Rock";
     } else if (playerSelection == "Scissors" && computerSelection == "Rock") { 
-    //  THEN winOrLose = ("You Lose! Rock beats Scissors")
-      return "You Lose! Rock beats Scissors";
-    //ELSE IF playerSelection is Paper and computerSelection is Scissors
-    } else if (playerSelection == "Paper" && computerSelection == "Scissors") { 
-      //  THEN winOrLose = ("You Lose! Scissors beats Paper")
-      return "You Lose! Scissors beats Paper";
-    //ELSE IF playerSelection is Paper and computerSelection is Rock
+      results =  "You Lose! Rock beats Scissors";
+    } else if (playerSelection == "Paper" && computerSelection == "Scissors") {
+      results = "You Lose! Scissors beats Paper";
+
+
+    //player Win
     } else if (playerSelection == "Paper" && computerSelection == "Rock") { 
-      //  THEN winOrLose = ("You Win! Paper beats Rock")
-      return "You Win! Paper beats Rock";
-    //ELSE IF playerSelection is Scissors and computerSelection is Paper
+      results = "You Win the Round! Paper beats Rock";
     } else if (playerSelection == "Scissors" && computerSelection == "Paper") { 
-      //  THEN winOrLose = ("You Win! Scissors beats Paper")
-      return "You Win! Scissors beats Paper";
-    //ELSE IF playerSelection is Rock and computerSelection is Scissors
+      results = "You Win the Round! Scissors beats Paper";
     } else if (playerSelection == "Rock" && computerSelection == "Scissors") { 
-      //  THEN winOrLose = ("You Win! Rock beats Scissors")
-      return "You Win! Rock beats Scissors";
-    //ELSE  winOrLose = ("It's a Tie! Both chose " + player Selection + "!")
+      results = "You Win the Round! Rock beats Scissors";
+
+      //player Tie 
     } else {
-      return  ("It's a Tie! Both chose " + playerSelection + "!");
-    //  ;
+      results = ("It's a Tie! Both chose " + playerSelection + "!");
+      //and it's a do over
+      rounds = --rounds;
     }
-   //End 
-   }
 
-
-
-
-   //   5 times Do This
-i = 0;
-while (i < 5) {  // 5 iterations, i = 0,1,2,3,4 that's five!
-  //New variable: get player Selection string argument for playOneRound using a prompt
-    let playerSelection = prompt("Rock, Paper, Scissors: shoot!", "Rock");
-  //New variable: computerSelection string argument for playOneRound 
-    let computerSelection = computerPlay(); 
-  // playOneRound(playerSelection, computerSelection);
-    let winOrLose = playOneRound(playerSelection, computerSelection);
-   // IF winOrLose > "You Win"
-    if (winOrLose > "You Win") { 
-   //     THEN score = ++score    ......ADD one to the Variable score's value;
-    score = ++score;        
+      //player scores on a Win
+    if (results > "You Win") { 
+      score = ++score;        
     }
-       // alert(winOrLose + "Your Score: " + score)  .....tell the result
-       alert(winOrLose + ". Your Score: " + score + "/5")
-   i++; //step up to the next iteration
-}
+   return [score,rounds,results];
+  }
 
 
+
+
+
+
+// remove the content of the divs and replace it with information 
+//    from the array output of playRound() 
+function updateDisplay(displayArray) {
+
+  //select div for results
+  const displayResults = document.querySelector('#displayResults'); //select div for results
+  //clear it out
+  displayResults.removeChild(displayResults.firstElementChild);
+ //add the new results
+  const newDivResults = document.createElement('div');
+  newDivResults.classList.add('divResults');
+  newDivResults.style.color = 'blue';    
+  newDivResults.textContent = displayArray[2];
+  displayResults.appendChild(newDivResults);
+
+//select div for score
+  const currentScore = document.querySelector('#currentScore'); //select div for score
+  //clear it out  
+  currentScore.removeChild(currentScore.firstElementChild);
+   //add the new results
+  const newScoreRounds = document.createElement('div');
+  newScoreRounds.classList.add('scoreRounds');
+  newScoreRounds.style.color = 'green';
+  newScoreRounds.textContent = (displayArray[0] + '/' + displayArray[1]);
+  currentScore.appendChild(newScoreRounds);
+
+  if (displayArray[1] === 1) {
+    const winner = document.querySelector('#winner'); //select div for score
+    //clear it out
+    winner.removeChild(winner.firstElementChild);
+
+    const newWhoWon = document.createElement('div');
+    newWhoWon.classList.add('whoWon');
+    newWhoWon.fontSize = '12px';
+    newWhoWon.textContent = "Who will be the Champion of five rounds?";
+    winner.appendChild(newWhoWon);
+  };
+  
+  if (displayArray[1] > 4) {
+    const winner = document.querySelector('#winner'); //select div for score
+    //clear it out
+    winner.removeChild(winner.firstElementChild);
+    const newWhoWon = document.createElement('div');
+    newWhoWon.fontSize = '20px';
+    newWhoWon.classList.add('whoWon');
+    if (displayArray[0] > 2){
+      newWhoWon.textContent = "You are the Champion!";
+    } else {
+      newWhoWon.textContent = "You were Defeated";
+    };
+    winner.appendChild(newWhoWon);
+  };
+  
+};
+//
+
+
+
+
+//make a node list called buttons (all of the element button)
+const buttons = document.querySelectorAll('button');
+
+//iterate through each button with method .forEach 
+buttons.forEach((button) => {
+
+    // to give each button(no 's') gets a click listener
+    button.addEventListener('click', () => {
+      updateDisplay(playRound(button.id, computerPlay()));
+      
+    });
+  });
+  
